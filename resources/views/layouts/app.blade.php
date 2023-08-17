1cb61915c9e0474787eaf06ddb8d94199016e5a1
PROPERTY\214843 <mintumia8@gmail.com>
2023-08-17 20:02:03 +0600

    Font Awesome Added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,19 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Main Page</title>
     <link rel="stylesheet" href="{{asset('plugins')}}/tailwindcss/main.css">
+    <link rel="stylesheet" href="{{asset('plugins')}}/fontawesome-free-6.4.2-web/css/all.css">
+   {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">--}}
+
 
 </head>
 <body class="container">
+{{--<div class="flex flex-wrap gap-4">
+    <i class="fa fa-check-square"></i>
+    <i class="fa fa-solid fa-download text-3xl text-blue-900"></i>
+    <i class="fa fa-solid fa-address-book text-3xl text-blue-900"></i>
+    <i class="fa fa-solid fa-map-location text-3xl text-blue-900"></i>
+    <i class="fa fa-solid fa-location-dot text-3xl text-blue-900"></i>
+</div>--}}
 @include('layouts.navbar')
 
 
